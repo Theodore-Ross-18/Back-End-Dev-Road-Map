8f16255f0271f079abdbbd6db12ecb8ea0dee991
Theodore Ross <theodoreross18@gmail.com>
2023-09-23 20:45:45 +0800

    Modified Operators

--- 09 Learn a Language/01 C#/08 Operators/Program.cs
+++ 09 Learn a Language/01 C#/08 Operators/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// Operators
+
+// int sum1 = 100 + 50;        // 150 (100 + 50)
+// int sum2 = sum1 + 250;      // 400 (150 + 250)
+// int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+// Console.WriteLine(sum1);
+// Console.WriteLine(sum2);
+// Console.WriteLine(sum3);
+
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int x = 5;
+      int y = 3;
+      Console.WriteLine(x++ + y--);  
+     }
+  }
+}
