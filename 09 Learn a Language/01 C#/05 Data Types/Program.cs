5c00899da3b51547eb02862a5bd82ad753b38faf
Theodore Ross <theodoreross18@gmail.com>
2023-09-23 14:40:00 +0800

    Type Casting

---
+++ 09 Learn a Language/01 C#/05 Data Types/Program.cs
@@ -0,0 +1,50 @@
+﻿
+// Data Types
+
+// int myNum = 5;               // Integer (whole number)
+// double myDoubleNum = 5.99D;  // Floating point number
+// char myLetter = 'D';         // Character
+// bool myBool = true;          // Boolean
+// string myText = "Hello";     // String
+
+// Numbers
+
+// Integer Types
+
+int myNum2 = 100000;
+Console.WriteLine(myNum2);
+
+// Long
+
+long myNum3 = 15000000000L;
+Console.WriteLine(myNum3);
+
+// Floating Point Types
+
+// Float
+float myNum4 = 5.75F;
+Console.WriteLine(myNum4);
+
+// Double
+double myNum5 = 19.99D;
+Console.WriteLine(myNum5);
+
+// Scientific Numbers
+float f1 = 35e3F;
+double d1 = 12E4D;
+Console.WriteLine(f1);
+Console.WriteLine(d1);
+
+// Booleans
+bool isCSharpFun = true;
+bool isFishTasty = false;
+Console.WriteLine(isCSharpFun);   // Outputs True
+Console.WriteLine(isFishTasty);   // Outputs False
+
+// Characters
+char myGrade = 'B';
+Console.WriteLine(myGrade);
+
+// Strings
+string greeting = "Hello World";
+Console.WriteLine(greeting);
