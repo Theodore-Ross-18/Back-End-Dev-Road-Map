a647d82d296a3ca2150a35878353f8a50fb788d1
Theodore Ross <theodoreross18@gmail.com>
2023-10-02 17:36:06 +0800

    Added and Changes

--- 09 Learn a Language/01 C#/07 User Input/Program.cs
+++ 09 Learn a Language/01 C#/07 User Input/Program.cs
@@ -1,19 +1,19 @@
-﻿// User Input
+﻿// // User Input
 
-// Type your username and press enter
-Console.Write("Enter username: ");
+// // Type your username and press enter
+// Console.Write("Enter username: ");
 
-// Create a string variable and get user input from the keyboard and store it in the variable
-string userName = Console.ReadLine();
+// // Create a string variable and get user input from the keyboard and store it in the variable
+// string userName = Console.ReadLine();
 
-// Error
-// Console.WriteLine("Enter your age:");
-// int age = Console.ReadLine();
+// // Error
+// // Console.WriteLine("Enter your age:");
+// // int age = Console.ReadLine();
+// // Console.WriteLine("Your age is: " + age);
+
+// // Use a method
+// Console.Write("Enter your age: ");
+// int age = Convert.ToInt32(Console.ReadLine());
+// // Print the value of the variable (userName), which will display the input value
+// Console.WriteLine("Username is: " + userName);
 // Console.WriteLine("Your age is: " + age);
-
-// Use a method
-Console.Write("Enter your age: ");
-int age = Convert.ToInt32(Console.ReadLine());
-// Print the value of the variable (userName), which will display the input value
-Console.WriteLine("Username is: " + userName);
-Console.WriteLine("Your age is: " + age);
