b15f1096bc31eb59d160a8990c4e0807f0e27f9b
Theodore Ross <theodoreross18@gmail.com>
2023-10-04 20:56:19 +0800

    Encapsulation

---
+++ 09 Learn a Language/01 C#/16 OPP/03 Encapsulation/Properties.cs
@@ -0,0 +1,21 @@
+﻿// C# Properties (Get and Set)
+
+class Person
+{
+  private string name; // field
+  public string Name   // property
+  {
+    get { return name; }
+    set { name = value; }
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Person myObj = new Person();
+    myObj.Name = "Theodore";
+    Console.WriteLine(myObj.Name);
+  }
+}
