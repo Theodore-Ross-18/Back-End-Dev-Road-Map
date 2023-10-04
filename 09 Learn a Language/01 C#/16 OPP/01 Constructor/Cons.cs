1a43971e93efd7a32c06ad6d5695c8b3d4387257
Theodore Ross <theodoreross18@gmail.com>
2023-10-04 20:26:25 +0800

    Changes

---
+++ 09 Learn a Language/01 C#/16 OPP/01 Constructor/Cons.cs
@@ -0,0 +1,25 @@
+﻿// C# Constructors 
+
+class Car
+{
+  public string model;
+  public string color;
+  public int year;
+
+  // Create a class constructor with multiple parameters
+  public Car(string modelName, string modelColor, int modelYear)
+  {
+    model = modelName;
+    color = modelColor;
+    year = modelYear;
+  }
+
+  static void Main(string[] args)
+  {
+    Car Ford = new Car("Mustang", "Red", 1969);
+    Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+  }
+}
+
+
+// Outputs Red 1969 Mustang
