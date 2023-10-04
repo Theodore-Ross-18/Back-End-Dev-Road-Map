33bedab6f15a782d32b45be8c86aae23c1d0745a
Theodore Ross <theodoreross18@gmail.com>
2023-10-04 17:43:38 +0800

    Changes

---
+++ 09 Learn a Language/01 C#/16 Classes/Classes.cs
@@ -0,0 +1,14 @@
+﻿// OOP 
+
+class Car 
+{
+  public string model = "Mustang";
+  public string color = "Black";
+  public int year = 1969;
+  public int maxSpeed = 200;
+  public void fullThrottle()
+  {
+    Console.WriteLine("The car is going as fast as it can!");
+  }
+}
+
---
+++ 09 Learn a Language/01 C#/16 Classes/Objects.cs
@@ -0,0 +1,15 @@
+
+class Objects
+{
+    static void Main(string[] args)
+  {
+    Car Ford = new Car();
+
+    Console.WriteLine($"Model: {Ford.model}");
+    Console.WriteLine($"Color: {Ford.color}");
+    Console.WriteLine($"Year: {Ford.year}");
+    Console.WriteLine($"Max-Speed: {Ford.maxSpeed}");
+    Ford.fullThrottle();
+  }
+
+}
