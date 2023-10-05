5ba8f36c6348cc6b8b2a6cf23cf342bd173d1845
Theodore Ross <theodoreross18@gmail.com>
2023-10-05 16:07:54 +0800

    Files

---
+++ 09 Learn a Language/01 C#/16 OOP/10 Files/System.cs
@@ -0,0 +1,7 @@
+﻿using System.IO;  // include the System.IO namespace
+
+string writeText = "Hello World!";  // Create a text string
+File.WriteAllText("filename.txt", writeText);  // Create a file and write the content of writeText to it
+
+string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
+Console.WriteLine(readText);  // Output the content
