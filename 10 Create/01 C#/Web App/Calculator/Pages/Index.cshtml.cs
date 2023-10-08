a5ca1c35062f67ad9ee234bfb32c341ab5c0ff7d
Theodore Ross <theodoreross18@gmail.com>
2023-10-08 22:26:27 +0800

    Calculator Web App Issue: C button does not work when user is done calculating

---
+++ 10 Create/01 C#/Web App/Calculator/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NCalc;
+
+namespace Calculator.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Expression { get; set; }
+        public string Result { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            try
+            {
+                Result = Calculate(Expression);
+            }
+            catch (Exception ex)
+            {
+                Result = "Error: " + ex.Message;
+            }
+        }
+
+        private string Calculate(string expression)
+        {
+            try
+            {
+                var evaluator = new Expression(expression);
+                var result = evaluator.Evaluate();
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Invalid expression: " + ex.Message);
+            }
+        }
+    }
+}
