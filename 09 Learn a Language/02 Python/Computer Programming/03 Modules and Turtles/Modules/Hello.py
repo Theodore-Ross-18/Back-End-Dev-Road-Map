3194f4c3599ed8eb68ffa9f27025798685dde75a
Theodore Ross <theodoreross18@gmail.com>
2023-10-25 23:50:31 +0800

    Modules

---
+++ 09 Learn a Language/02 Python/Computer Programming/03 Modules and Turtles/Modules/Hello.py
@@ -0,0 +1,9 @@
+
+def message(text):
+    print("Hello " + text)
+
+def add(x,y):
+    return x+y
+
+def greetings(message):
+    print("Welcome to Python Module" + message)
---
+++ 09 Learn a Language/02 Python/Computer Programming/03 Modules and Turtles/Modules/Import.py
@@ -0,0 +1,6 @@
+import Hello
+
+print(Hello.message("I'm Theodore"))
+print(Hello.add(100,5))
+
+Hello.greetings("Theyy")
