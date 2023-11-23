dbf2082ef63c0b9b4ab86d765a625067f8a2d421
Theodore Ross <theodoreross18@gmail.com>
2023-11-23 11:52:07 +0800

    Ford

--- 09 Learn a Language/01 C#/16 OOP/05 Inheritance/Inherit.cs
+++ 09 Learn a Language/01 C#/16 OOP/05 Inheritance/Inherit.cs
@@ -1,28 +1,44 @@
-﻿class Vehicle  // base class (parent) 
+﻿using System;
+
+class Vehicle  // base class (parent) 
 {
-  public string brand = "Ford";  // Vehicle field
-  public void honk()             // Vehicle method 
-  {                    
-    Console.WriteLine("Tuut, tuut!");
-  }
+    public string brand = "Ford";  // Vehicle field
+    public void honk()             // Vehicle method 
+    {
+        Console.WriteLine("Tuut, tuut!");
+    }
 }
 
 class Car : Vehicle  // derived class (child)
 {
-  public string modelName = "Mustang";  // Car field
+    public string modelName = "Mustang";  // Car field
+}
+
+class FordCar : Vehicle  // another derived class (child)
+{
+    public string modelName = "Falcon";  // FordCar field
 }
 
 class Program
 {
-  static void Main(string[] args)
-  {
-    // Create a myCar object
-    Car myCar = new Car();
+    static void Main(string[] args)
+    {
+        // Create a myCar object
+        Car myCar = new Car();
 
-    // Call the honk() method (From the Vehicle class) on the myCar object
-    myCar.honk();
+        // Call the honk() method (From the Vehicle class) on the myCar object
+        myCar.honk();
 
-    // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
-    Console.WriteLine(myCar.brand + " " + myCar.modelName);
-  }
+        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+        Console.WriteLine(myCar.brand + " " + myCar.modelName);
+
+        // Create a fordCar object
+        FordCar fordCar = new FordCar();
+
+        // Call the honk() method (From the Vehicle class) on the fordCar object
+        fordCar.honk();
+
+        // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the FordCar class
+        Console.WriteLine(fordCar.brand + " " + fordCar.modelName);
+    }
 }
